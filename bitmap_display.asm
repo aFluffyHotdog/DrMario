007715da5ea6544ed5c910675aae7c9361569d21
Thitiwut Pattanasuttinont <93137058+aFluffyHotdog@users.noreply.github.com>
2024-11-10 20:02:33 -0500

    Drew background

--- bitmap_display.asm
+++ bitmap_display.asm
@@ -20,20 +20,77 @@ ADDR_DSPL:
 	.globl main
 
 main:
-    li $t1, 0xff0000        # $t1 = red
-    li $t2, 0x00ff00        # $t2 = green
-    li $t3, 0x0000ff        # $t3 = blue
+        addi $t9, $zero, 24     # we are looping 24 times
+        addi $t2, $zero, 0      # $t2 = loop variable i
+        li $t1, 0x666666        # $t1 = grey
+        lw $t0, ADDR_DSPL       # $t0 = base address for display
+        addi $t0, $t0, 528      # add initial offset of x = right 4, y = down 4
 
-    lw $t0, ADDR_DSPL       # $t0 = base address for display
-    sw $t1, 0($t0)          # paint the first unit (i.e., top-left) red  
-    #store whatever is in t1, into the address at 0
-    sw $t2, 4($t0)          # paint the second unit on the first row green
-    # each pixel is an int so it takes 4 bytes, so we need to move 4 bytes to paint the next spot
-    add $t4, $t1, $t2 # t4 = t1 + t2
-    sw $t4, 8($t0)          # paint the second unit on the first row green
-    add $t5, $t1, $t3
-    sw $t5, 12($t0)
-    sw $t3, 128($t0)        # paint the first unit on the second row blue
+START:  # setup to draw left border
+        beq $t2, $t9, START2    # terminate loop when t2 = t9 or i = 24
+UPDATE: 
+        addi $t0, $t0, 128      # we shift where we write by 128 bits
+        addi $t2, $t2, 1        # increment our loop variable
+        sw $t1, 0($t0)          # we draw a grey square
+        j START
+
+
+START2: # setup to draw bottom border
+        addi $t2, $zero, 0      # reset $t2 back to 0
+        addi $t9, $zero, 16     # will only loop 16 times
+        j LOOP2
+LOOP2:
+        beq $t2, $t9, START3    # terminate when i = 16
+UPDATE2: 
+        addi $t0, $t0, 4        # we shift where we write by 4 bits (+ 1 to the right)
+        addi $t2, $t2, 1        # increment our loop variable
+        sw $t1, 0($t0)
+        j LOOP2
+        
+        
+START3: # setup to draw right border
+        addi $t2, $zero, 0      #reset $t2 back to 0
+        addi $t9, $zero, 23     #will only loop 23 times (one pixelis already drawn in so we draw 1 less time)
+        j LOOP3
+LOOP3:
+        beq $t2, $t9, START4      #terminate when i = 24
+UPDATE3: 
+        addi $t0, $t0, -128     #we shift where we write by 4 bits
+        addi $t2, $t2, 1        #increment our loop variable
+        sw $t1, 0($t0)
+        j LOOP3
+        
+START4: # setup to draw right border
+        addi $t2, $zero, 0      #reset $t2 back to 0
+        addi $t9, $zero, 6     #will only loop 23 times
+        j LOOP4
+LOOP4:
+        beq $t2, $t9, START5      #terminate when i = 24
+UPDATE4: 
+        addi $t0, $t0, -4     #we shift where we write by 4 bits
+        addi $t2, $t2, 1        #increment our loop variable
+        sw $t1, 0($t0)
+        j LOOP4
+        
+START5: # setup to draw top left part + bottle neck
+        addi $t0, $t0, -128	
+        sw $t1, 0($t0)		#drew right lip
+        addi $t0, $t0, 128
+        addi $t2, $zero, 0      #reset $t2 back to 0
+        addi $t9, $zero, 6      #will only loop 6 times
+        addi $t0, $t0, -16      #jump over the bottle neck
+        addi $t0, $t0, -128
+        sw $t1, 0($t0)		#drew left lip
+        addi $t0, $t0, 128
+        sw $t1, 0($t0)
+        j LOOP5
+LOOP5:
+        beq $t2, $t9, exit      #terminate when i = 24
+UPDATE5: 
+        addi $t0, $t0, -4     #we shift where we write by 4 bits
+        addi $t2, $t2, 1        #increment our loop variable
+        sw $t1, 0($t0)
+        j LOOP5
 exit:
-    li $v0, 10              # terminate the program gracefully
+    li $v0, 10                  # terminate the program gracefully
     syscall
